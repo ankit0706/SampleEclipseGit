7235067407c14ffc9d08d9178e729f72af903c1c
AANU <AANU@INBA5CG8293VJ9.inbe.capco.com>
2020-08-27 11:06:50 +0530

    Committing program to local git repository

---
+++ src/com/sample/plugintest/EGitPluginTest.java
@@ -0,0 +1,8 @@
+package com.sample.plugintest;
+
+public class EGitPluginTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Smaple program for testing gt operations directly from Eclipse using EGit plugin");
+	}
+}
